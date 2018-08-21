1e5012ef0fa165bb84e4c8f2f4b4a2d1c9f70b52
Ankit Rana <ankitrana@arana-0145.local>
2018-08-21 12:02:29 -0400

    Creating initial schema for nasa

---
+++ ckanext/custom_schema/plugins.py
@@ -0,0 +1,16 @@
+from ckan.plugins import toolkit, IConfigurer, SingletonPlugin, implements
+
+class customSchema(SingletonPlugin):
+    implements(IConfigurer)
+
+    def update_config(self, config):
+        toolkit.add_public_directory(config, "static")
+        toolkit.add_template_directory(config, "templates")
+
+        config['scheming.presets'] = """
+ckanext.scheming:presets.json
+"""
+
+        config['scheming.dataset_schemas'] = """
+ckanext.custom_schema:schemas/dataset.yaml
+"""
