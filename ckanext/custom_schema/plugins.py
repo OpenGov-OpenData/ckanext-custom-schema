ff1bff3ec6faa42155cd2dea24815be79e7a256b
javier-tello23 <49450112+javier-tello23@users.noreply.github.com>
2019-10-18 11:58:33 -0400

    [PA-842]Update CDT schema (#6)
    * Added required metadatafields + added dropdown for Topic in adding dataset page

--- ckanext/custom_schema/plugins.py
+++ ckanext/custom_schema/plugins.py
@@ -1,8 +1,9 @@
-from ckan.plugins import toolkit, IConfigurer, IRoutes, SingletonPlugin, implements
+from ckan.plugins import toolkit, IConfigurer, IRoutes, IPackageController, SingletonPlugin, implements, toolkit
 
 class customSchema(SingletonPlugin):
     implements(IConfigurer)
     implements(IRoutes,inherit=True)
+    implements(IPackageController, inherit=True)
 
     def update_config(self, config):
         toolkit.add_public_directory(config, "static")
@@ -25,4 +26,34 @@ ckanext.custom_schema:schemas/dataset.yaml
             action='metadata_download'
         )
         return m
+
+    # IPackageController
+    def after_create(self, context, pkg_dict):
+        if 'group' in pkg_dict:
+            if pkg_dict['group']:
+                data = {
+                    'id': pkg_dict['group'],
+                    'object': pkg_dict['id'],
+                    'object_type': 'package',
+                    'capacity': 'public'
+                }
+                toolkit.get_action('member_create')(context, data)
+
+    def after_update(self, context, pkg_dict):
+        if 'group' in pkg_dict:
+            if pkg_dict['group']:
+                data = {
+                    'id': pkg_dict['group'],
+                    'object': pkg_dict['id'],
+                    'object_type': 'package',
+                    'capacity': 'public'
+                }
+                toolkit.get_action('member_create')(context, data)
+            self.remove_from_other_groups(context, pkg_dict['id'])
+
+    def remove_from_other_groups(self, context, package_id):
+        package = toolkit.get_action('package_show')(context, {'id': package_id})
+        for group in package['groups']:
+            if group['name'] != package['group']:
+                toolkit.get_action('member_delete')(context, {'id': group['id'], 'object': package['id'], 'object_type': 'package'})
 
