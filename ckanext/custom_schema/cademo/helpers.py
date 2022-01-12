6b29898e54f5258eebd86a5d5dbcd4f96a25042f
Jay Guo <jguo144@gmail.com>
2022-01-12 11:18:37 -0500

    Add latest CDT schema (#12)
    * Add latest CDT schema to master

---
+++ ckanext/custom_schema/cademo/helpers.py
@@ -0,0 +1,26 @@
+import ckan.plugins as p
+
+get_action = p.toolkit.get_action
+ValidationError = p.toolkit.ValidationError
+
+def get_group_list():
+    try:
+        data_dict_global_results = {
+            'all_fields': True,
+            'sort': 'title asc',
+            'type': 'group',
+        }
+        global_results = get_action('group_list')({}, data_dict_global_results)
+        return global_results
+    except ValidationError:
+        return []
+
+def get_selected_group(data):
+    try:
+        group_list = data.get('groups', [])
+        for item in group_list:
+            if item.get('name'):
+                return item.get('name')
+    except (TypeError, ValueError):
+        pass
+    return ''
